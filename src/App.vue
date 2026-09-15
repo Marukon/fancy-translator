fc104872d1e44039e30e10dc2b631dd7fb8ba5b6
Maruko <6285343+Marukon@users.noreply.github.com>
2026-09-15 15:51:29 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ useDarkMode()
       >
         <Logo :stroke-width="1.25" class="text-0.875em" />
       </div>
-      Bubu Fanyi
+      布布翻译
     </h1>
     <div class="flex gap-2 items-center">
       <SettingsModal />
